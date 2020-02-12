6c1cce6d1bbe2d82cb28d441bd66fd19ba6e0f81
Czeglédi Attila <czegiati@gmail.com>
2020-02-12 16:42:46 +0100

    package 1.0

--- src/main/java/hu/exth/l4tgt/model/Textures/Texture.java
+++ src/main/java/hu/exth/l4tgt/model/Textures/Texture.java
@@ -1,35 +1,22 @@
 package hu.exth.l4tgt.model.Textures;
-import javafx.scene.image.Image;
 
-public class Texture {
+
+
+
+public abstract class Texture {
 	private String textureGroup;
 	private String textureName;
-	private Image texture;
 	private String URL;
 	private int Id;
 	
-	public Texture(String textureName,Image texture,boolean iseffect,int id) {
-		this.texture=texture;
-		this.textureName=textureName;
-		this.URL=texture.getUrl();
-		this.Id=id;
-		this.textureGroup="Other";
-	}
-	
-	public Texture(int id,String group,String textureName,String path,boolean iseffect,boolean isicon,boolean isspecial) {
-		this.texture=new Image(path);
-		this.URL=path;
-		this.textureName=textureName;
-		this.Id=id;
-		if(group==null) {this.textureGroup="Other";} else this.textureGroup=group;
-	}
+
+
 	
 	public Texture() {}
 	public String getTextureName() {return textureName;}
 	public void setTextureName(String t) {this.textureName=t;}
 	
-	public Image getTexture() {return texture;};
-	public void setTexture(Texture t) {this.texture=t.texture; this.URL=texture.getUrl();}
+
 
 	public int getId() {return Id;};
 	public void setId(int newi) {this.Id=newi;};
@@ -38,6 +25,6 @@ public class Texture {
 	public void setTextureGroup(String g) {this.textureGroup=g;}
 	
 	public String getURL() {return this.URL;}
-	public void setURL(String url) {this.texture=new Image(url); this.URL=url;}
+
 
 }
